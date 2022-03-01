ea81f029bb9696973abec411b39fb3c8e2ce85d0
Nelson David Apraez Burbano <Napraez@intergrupo.com>
2022-02-28 20:01:50 -0500

    Unit test added

--- WageringRoulette.UnitTest/ApplicationService/WageringRouletteApplicationServiceTest.cs
+++ WageringRoulette.UnitTest/ApplicationService/WageringRouletteApplicationServiceTest.cs
@@ -1,8 +1,10 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using WageringRoulette.ApplicationServices;
 using WageringRoulette.ApplicationServices.Exceptions;
+using WageringRoulette.ApplicationServices.Requests;
 using WageringRoulette.DomainServices.Abstraction;
 using WageringRoulette.DomainServices.Model;
 using Xunit;
@@ -127,9 +129,7 @@ namespace WageringRoulette.UnitTest
 
             mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>()));
             mockDomainServices.Setup(x => x.GetById(id)).Returns(rouletteModel);
-
             var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
-
             BusinessException ex = Assert.Throws<BusinessException>(() => instance.CloseWager(id));
 
             Assert.Equal(GetErrorDescription(MessageCodes.NotAllowedCloseByOpen), ex.Message);
@@ -151,12 +151,116 @@ namespace WageringRoulette.UnitTest
 
             mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>()));
             mockDomainServices.Setup(x => x.GetById(id)).Returns(rouletteModel);
-
             var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
-
             BusinessException ex = Assert.Throws<BusinessException>(() => instance.CloseWager(id));
 
             Assert.Equal(GetErrorDescription(MessageCodes.NotAllowedClose), ex.Message);
+        }
+
+        [Fact]
+        public void Wager_Should_ErrorMoneyOutRange()
+        {
+            string id = Guid.NewGuid().ToString();
+
+            WagerRequest wagerRequest = new WagerRequest()
+            {
+                Money = 0
+            };
+
+            RouletteModel rouletteModel = new RouletteModel()
+            {
+                Id = id,
+                ClosingDate = "2022-02-28T10:01:54.9571247Z",
+                IsOpen = true,
+                OpeningDate = "2022-02-28T10:01:54.9571247Z",
+                Pocket = new IDictionary<string, decimal>[40]
+            };
+
+            mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>()));
+            mockDomainServices.Setup(x => x.GetById(id)).Returns(rouletteModel);
+            var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
+            BusinessException ex = Assert.Throws<BusinessException>(() => instance.Wager(id, wagerRequest));
+
+            Assert.Equal(GetErrorDescription(MessageCodes.MoneyOutRange), ex.Message);
+        }
+
+        [Fact]
+        public void Wager_Should_ErrorNotFound()
+        {
+            string id = Guid.NewGuid().ToString();
+
+            WagerRequest wagerRequest = new WagerRequest()
+            {
+                Money = 10,
+                Position = 1,
+                UserId = "Usuario"
+            };
+            mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>()));
+            var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
+            BusinessException ex = Assert.Throws<BusinessException>(() => instance.Wager(id, wagerRequest));
+
+            Assert.Equal(GetErrorDescription(MessageCodes.NotFound), ex.Message);
+        }
+
+        [Fact]
+        public void Wager_Should_ErrorRouletteClosed()
+        {
+            string id = Guid.NewGuid().ToString();
+
+            WagerRequest wagerRequest = new WagerRequest()
+            {
+                Money = 10,
+                Position = 1,
+                UserId = "Usuario"
+            };
+
+            RouletteModel rouletteModel = new RouletteModel()
+            {
+                Id = id,
+                ClosingDate = "2022-02-28T10:01:54.9571247Z",
+                IsOpen = false,
+                OpeningDate = "2022-02-28T10:01:54.9571247Z",
+                Pocket = new IDictionary<string, decimal>[40]
+            };
+
+            mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>()));
+            mockDomainServices.Setup(x => x.GetById(id)).Returns(rouletteModel);
+            var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
+            BusinessException ex = Assert.Throws<BusinessException>(() => instance.Wager(id, wagerRequest));
+
+            Assert.Equal(GetErrorDescription(MessageCodes.RouletteClosed), ex.Message);
+        }
+
+        [Fact]
+        public void Wager_Should_Wagered()
+        {
+            string id = Guid.NewGuid().ToString();
+
+            WagerRequest wagerRequest = new WagerRequest()
+            {
+                Money = 10,
+                Position = 1,
+                UserId = "Usuario"
+            };
+            RouletteModel rouletteModel = new RouletteModel()
+            {
+                Id = id,
+                ClosingDate = null,
+                IsOpen = true,
+                OpeningDate = "2022-02-28T10:01:54.9571247Z",
+                Pocket = new IDictionary<string, decimal>[40]
+            };
+            for (int i = 0; i < rouletteModel.Pocket.Length; i++)
+            {
+                rouletteModel.Pocket[i] = new Dictionary<string, decimal>();
+            }
+
+            mockDomainServices.Setup(x => x.Update(It.IsAny<RouletteModel>())).Returns(rouletteModel);
+            mockDomainServices.Setup(x => x.GetById(id)).Returns(rouletteModel);
+            var instance = new WageringRouletteApplicationServie(mockDomainServices.Object);
+            var result = instance.Wager(id, wagerRequest);
+
+            Assert.True(result.Pocket[1].Any());
         }
     }
 }
